afa974e936372aaeeb7260f5b424a6b9a99fc803
firmangaluhs <firmangaluh66@gmail.com>
2020-08-26 12:35:54 +0700

    Menambahkan detail produk

---
+++ app/src/main/java/com/magang/jvm_es3_kula/ui/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.magang.jvm_es3_kula.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.magang.jvm_es3_kula.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
